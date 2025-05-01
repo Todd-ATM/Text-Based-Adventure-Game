b8f61eab4ca4e081c26201a11596ae4f4c31076c
Todd-ATM <ToddJr.Acrossthemountain@gmail.com>
2025-04-30 23:54:18 -0700

    Mistake, this time I added headers folder with game.h file as well as game.cpp into src

---
+++ headers/Game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_HEADER_H
+#define GAME_HEADER_H
+
+#include <iostream>
+
+class Game {
+  public:
+    void start();
+};
+
+#endif
---
+++ src/Game.cpp
@@ -0,0 +1,5 @@
+#include "Game.h"
+
+void Game::start() {
+    std::cout << "The game has started" << std::endl;
+}
