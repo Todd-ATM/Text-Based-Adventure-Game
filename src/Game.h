fd70ddfe03b5c1db1399be7830acc8314776d565
Todd-ATM <ToddJr.Acrossthemountain@gmail.com>
2025-04-30 22:51:38 -0700

    Added Src folder for source code, added main.cpp and Game.h to test

---
+++ src/Game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_HEADER_H
+#define GAME_HEADER_H
+
+#include <iostream>
+
+class Game {
+  public:
+    void test() {
+        std::cout << "This test is working great job linking" << std::endl;
+    }
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "Game.h"
+
+int main() {
+    Game game;
+
+    game.test();
+
+    return 0;
+}
